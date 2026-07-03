b1c5cffe4dcc0aa4237048744657a39938d25c58
Bro-coder01 <ally185512@gmail.com>
2026-07-03 18:21:22 +0000

    task4 done

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module provides a function to convert an object to a JSON string.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns the object from json."""
+    return json.louds(my_str)
