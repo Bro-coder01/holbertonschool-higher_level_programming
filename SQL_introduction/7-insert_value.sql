a2e4119ce349eb5ba6e6cc9c2995251423e1437e
Bro-coder01 <ally185512@gmail.com>
2026-07-23 17:32:18 +0300

    Update 7-insert_value.sql

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 --  inserts a new row in the table first_table (database hbtn_0c_0)
-INSERT id=89 ,name="Best School" INTO first_table
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
