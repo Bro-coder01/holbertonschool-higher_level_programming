2be6a26855be40843b646e96c65d33b2e43ec04b
Bro-coder01 <ally185512@gmail.com>
2026-07-25 23:51:33 +0300

    Create 2-my_filter_states.py

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Filter states using a name supplied by the user."""
+
+import sys
+
+import MySQLdb
+
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+    )
+
+    cursor = connection.cursor()
+
+    query = (
+        "SELECT id, name FROM states "
+        "WHERE name LIKE BINARY '{}' "
+        "ORDER BY states.id ASC"
+    ).format(sys.argv[4])
+
+    cursor.execute(query)
+
+    for state in cursor.fetchall():
+        print(state)
+
+    cursor.close()
+    connection.close()
