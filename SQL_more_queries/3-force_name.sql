8683ddebe1aec2bad73e19bd22e597382ca8dc4c
Bro-coder01 <ally185512@gmail.com>
2026-07-25 23:16:41 +0300

    Create 3-force_name.sql

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- Creates the force_name table with a required name column.
+
+-- Creates the table if it does not already exist.
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
