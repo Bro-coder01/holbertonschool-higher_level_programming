fb820a64cb82e95cdad2877e3e40f08bd7d54d6f
Bro-coder01 <ally185512@gmail.com>
2026-08-08 17:37:21 +0300

    task6 done

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let arrs = ['C is fun','Python is cool','JavaScript is amazing'];
+const arrs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let cunt = 0; cunt < arrs.length; cunt++) {
-    console.log(arrs[cunt]);
+  console.log(arrs[cunt]);
 }
