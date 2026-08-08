223f7a5f2c412e08a23cf6d35de8e3089cc12f69
Bro-coder01 <ally185512@gmail.com>
2026-08-08 20:26:07 +0300

    task9 done

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-let v1 = parseInt(process.argv[2], 10), v2 = parseInt(process.argv[3], 10);
 function add(a, b) {
     return a + b;
 }
-
-if (isNaN(v1) || isNaN(v2)) {
-  console.log('Invalid input');
-} else {
-console.log(add(a = v1, b = v2));
-  }
-  
+console.log(add(parseInt(process.argv[2], 10), parseInt(process.argv[3], 10)));  
