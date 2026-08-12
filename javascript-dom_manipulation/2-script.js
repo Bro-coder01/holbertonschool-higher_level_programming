2a41e98d3ef66c75eb14ead9b43c77a03303888d
Bro-coder01 <ally185512@gmail.com>
2026-08-13 01:52:01 +0300

    done

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,5 @@
+const headrcolupd = document.querySelector('header');
+const botn = document.querySelector('#red_header');
+botn.addEventListener('click', function() {
+    headrcolupd.classList.add('red');
+});
